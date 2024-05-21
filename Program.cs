cb27f8ba06ac4285d51a13962d2c9f005c643a12
Joshua Smith (1000125321) <SMITJA21@student.op.ac.nz>
2024-05-21 15:14:53 +1200

    First Upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace vg_the_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
