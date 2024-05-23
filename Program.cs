8a1b822a98760c0b2bb937085cbb319e53c500b0
Daniel Tutty (1000128301) <TUTTDJ1@student.op.ac.nz>
2024-05-23 16:20:16 +1200

    Adding more room code

--- Program.cs
+++ Program.cs
@@ -32,12 +32,13 @@
         static void Room1()
         {
             Console.WriteLine("You are in Room 1");
-            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
                 case 1:
-                    Console.WriteLine("Your are already here");
+                    Console.WriteLine("Your are already here\n");
+                    Room1();
                     break;
                 case 2:
                     Room2();
@@ -47,6 +48,15 @@
                     break;
                 case 4:
                     Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
                     break;
             }
         }
@@ -54,7 +64,7 @@
         static void Room2()
         {
             Console.WriteLine("You are in Room 2");
-            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -62,13 +72,23 @@
                     Room1();
                     break;
                 case 2:
-                    Console.WriteLine("You are already here");
+                    Console.WriteLine("You are already here\n");
+                    Room2();
                     break;
                 case 3:
                     Room3();
                     break;
                 case 4:
                     Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
                     break;
             }
         }
@@ -76,7 +96,7 @@
             static void Room3()
         {
             Console.WriteLine("You are in Room 3");
-            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -87,10 +107,20 @@
                     Room2();
                     break;
                 case 3:
-                    Console.WriteLine("You are already here");
+                    Console.WriteLine("You are already here\n");
+                    Room3();
                     break;
                 case 4:
                     Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
                     break;
             }
         }
@@ -98,7 +128,7 @@
         static void Room4()
         {
             Console.WriteLine("You are in Room 4");
-            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -112,7 +142,110 @@
                     Room3();
                     break;
                 case 4:
-                    Console.WriteLine("You are already here");
+                    Console.WriteLine("You are already here\n");
+                    Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
+                    break;
+            }
+        }
+        static void Hallway()
+        {
+            Console.WriteLine("You are in the hallway");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Room1();
+                    break;
+                case 2:
+                    Room2();
+                    break;
+                case 3:
+                    Room3();
+                    break;
+                case 4:
+                    Room4();
+                    break;
+                case 5:
+                    Console.WriteLine("You are already here\n");
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
+                    break;
+            }
+        }
+        static void JanitorsCloset()
+        {
+            Console.WriteLine("You are in you are in the Janitors Closet");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Room1();
+                    break;
+                case 2:
+                    Room2();
+                    break;
+                case 3:
+                    Room3();
+                    break;
+                case 4:
+                    Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    Console.WriteLine("You are already here\n");
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Lift();
+                    break;
+            }
+        }
+        static void Lift()
+        {
+            Console.WriteLine("You are in the Lift");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\nRoom 4\nHallway\nJanitorsCloset\nLift");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Room1();
+                    break;
+                case 2:
+                    Room2();
+                    break;
+                case 3:
+                    Room3();
+                    break;
+                case 4:
+                    Room4();
+                    break;
+                case 5:
+                    Hallway();
+                    break;
+                case 6:
+                    JanitorsCloset();
+                    break;
+                case 7:
+                    Console.WriteLine("You are already here\n");
+                    Lift();
                     break;
             }
         }
