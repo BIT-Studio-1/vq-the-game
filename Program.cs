3d78e3949c5f3577c26cd142f5b777d5fd22a61f
Daniel Tutty (1000128301) <TUTTDJ1@student.op.ac.nz>
2024-05-23 15:56:03 +1200

    fixed rooms

--- Program.cs
+++ Program.cs
@@ -17,8 +17,83 @@
             Console.WriteLine("Just before you leave to go home to the 'Space Station' which is what you refer to as your house as.\nYou realise you are missing your trusty bottle of gin");
             Thread.Sleep(2000);
             Console.WriteLine("Your mission. Find your bottle of Gin!\nSo you can go home and have a great weekend!");
-            Console.WriteLine("Do you want to enter the room(1)?");
-            Console.WriteLine("Do you want to enter the room(1,2,3,4)?");
+            Console.WriteLine("Press enter to start");
+            Console.ReadLine();
+            Room1();
+
+        }
+
+
+        static void Room1()
+        {
+            Console.WriteLine("You are in Room 1");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Your are already here");
+                    break;
+                case 2:
+                    Room2();
+                    break;
+                case 3:
+                    Room3();
+                    break;
+                case 4:
+                    Room4();
+                    break;
+            }
+        }
+
+        static void Room2()
+        {
+            Console.WriteLine("You are in Room 2");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Room1();
+                    break;
+                case 2:
+                    Console.WriteLine("You are already here");
+                    break;
+                case 3:
+                    Room3();
+                    break;
+                case 4:
+                    Room4();
+                    break;
+            }
+        }
+
+            static void Room3()
+        {
+            Console.WriteLine("You are in Room 3");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Room1();
+                    break;
+                case 2:
+                    Room2();
+                    break;
+                case 3:
+                    Console.WriteLine("You are already here");
+                    break;
+                case 4:
+                    Room4();
+                    break;
+            }
+        }
+
+        static void Room4()
+        {
+            Console.WriteLine("You are in Room 4");
+            Console.WriteLine("Room 1\nRoom 2\nRoom 3\n Room 4");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -32,57 +107,7 @@
                     Room3();
                     break;
                 case 4:
-                    Room4();
-                    break;
-            }
-
-        }
-
-
-        static void Room1()
-        {
-            Console.WriteLine("Do you want to leave the room(1)?");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Room1();
-                    break;
-            }
-        }
-
-        static void Room2()
-        {
-            Console.WriteLine("Do you want to leave the room(1)?");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Room1();
-                    break;
-            }
-        }
-
-            static void Room3()
-        {
-            Console.WriteLine("Do you want to leave the room(1)?");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Room1();
-                    break;
-            }
-        }
-
-        static void Room4()
-        {
-            Console.WriteLine("Do you want to leave the room(1)?");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Room1();
+                    Console.WriteLine("You are already here");
                     break;
             }
         }
