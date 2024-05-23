6c8ae98d0cdc4c9336b6a331536292f9b9249b94
Matthew39099 <VISSMJ1@student.op.ac.nz>
2024-05-23 15:13:35 +1200

    First room

--- Program.cs
+++ Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
+        static void room1()
+        {
+            Console.WriteLine("Do you want to leave the room(1)?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("congrats u won!");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Do you want to enter the room(1)?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                room1();
+            }
         }
     }
 }
