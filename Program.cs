1169b836b78dea55b92494ad2a7051a32561d48c
Joshua Smith (1000125321) <SMITJA21@student.op.ac.nz>
2024-05-23 15:38:02 +1200

    First lot of intro dialogue created

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@
 
         static void Start()
         {
+            Console.WriteLine("You are the one and only Vaughn Malkin. \nYou wake up in a cold sweat, first year math exams cover you. ");
+            Thread.Sleep(1000);
+            Console.WriteLine("Just before you leave to go home to the 'Space Station' which is what you refer to as your house as.\nYou realise you are missing your trusty bottle of gin");
+            Thread.Sleep(2000);
+            Console.WriteLine("Your mission. Find your bottle of Gin! \n So you can go home and have a great weekend!");
             Console.WriteLine("Do you want to enter the room(1)?");
             int choice = Convert.ToInt32(Console.ReadLine());
             if (choice == 1)
